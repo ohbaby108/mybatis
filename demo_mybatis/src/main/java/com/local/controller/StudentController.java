aaa911e8577fb33b248dae3e33425ab4b282bc65
${ohbaby108} <${1084171538@qq.com}>
2020-12-05 15:51:07 +0800

    单表的增删改查

--- demo_mybatis/src/main/java/com/local/controller/StudentController.java
+++ demo_mybatis/src/main/java/com/local/controller/StudentController.java
@@ -1,20 +1,35 @@
 package com.local.controller;
 
 import com.local.bean.Student;
-import com.local.mapper.StudentMapper;
 import com.local.server.StudentServer;
+
+import java.util.List;
 
 public class StudentController {
     private StudentServer server=new StudentServer();
 
+    //学生的增加
     public void insertStudent(Student student){
         server.insert(student);
     }
 
+    //学生的删除
     public void deleteStudent(Integer sid){
         server.delete(sid);
     }
 
+    //学生的修改
+    public void updateStudent(Student student){
+        server.update(student);
+    }
 
+    //查询学生信息
+    public Student selectOne(Integer sid){
+        return server.selectOne(sid);
+    }
 
+    //查询所有学生
+    public List<Student> selectAll(){
+        return server.selectList();
+    }
 }
--- demo_mybatis/src/main/java/com/local/server/StudentServer.java
+++ demo_mybatis/src/main/java/com/local/server/StudentServer.java
@@ -3,16 +3,34 @@ package com.local.server;
 import com.local.bean.Student;
 import com.local.mapper.StudentMapper;
 
+import java.util.List;
+
 public class StudentServer {
 
     private StudentMapper mapper=new StudentMapper();
-
+    //增
     public void insert(Student student){
         mapper.insert(student);
     }
+
+    //删
     public void delete(Integer sid){
         mapper.delete(sid);
     }
 
+    //改
+    public void update(Student student){
+        mapper.update(student);
+    }
+
+    //查单个
+    public Student selectOne(Integer sid){
+        return mapper.selectOne(sid);
+    }
+
+    //查询全部
+    public List<Student> selectList(){
+        return mapper.selectList();
+    }
 
 }
